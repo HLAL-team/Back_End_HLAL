deed2171effec3e82ff6f5a11e95bef9f41ca64e
8Fadhil <fadhil0803@gmail.com>
2025-04-19 10:45:02 +0700

    first commit project hlal spring boot

---
+++ hlal/src/test/java/com/example/hlal/HlalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hlal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
